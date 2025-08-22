1e08f6e070e28862e25ce600a1771a3b02dd1f14
mayu <ykmshiss@gmail.com>
2025-08-22 13:07:47 +0900

    作成画面

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
-require './db/todos'
 require 'sinatra'
+require 'sqlite3'
+
+DB_PATHS = {
+  'development' => 'db/development.sqlite3',
+  'test' => 'db/test.sqlite3'
+}
+ENV['RACK_ENV'] ||= 'development'
+DB = SQLite3::Database.new(DB_PATHS[ENV['RACK_ENV']])
+
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+SQL
 
 get '/todos' do
-  @todos = DB.execute('SELECT title FROM todos').map(&:first)
+  @todos = DB.execute('SELECT title FROM todos').map { |row| row[0] }
+  erb :todos
+end
+
+
+post '/todos' do
+  DB.execute('INSERT INTO todos (title) VALUES (?)', [params[:title]])
+  redirect '/'
+end
+
+
+get '/' do
+  @todos = DB.execute('SELECT title FROM todos').map { |row| row[0] }
   erb :todos
 end
